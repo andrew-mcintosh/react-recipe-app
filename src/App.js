476899d4137414af72a36df7a3b6e30807410b2b
andrew-mcintosh <andrewdavidmcintosh@gmail.com>
2019-10-18 14:24:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return <div>hello from our recipe app </div>;
+  }
+}
+
+export default App;
