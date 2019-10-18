79f6b99de63ac79ba74b1f55394a3cb87ecb357a
andrew-mcintosh <andrewdavidmcintosh@gmail.com>
2019-10-18 15:11:14 -0400

    navbar routing created

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <h4>Hello from Home page</h4>;
+  }
+}
+
+export default Home;
---
+++ src/pages/Recipe.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    return <h4>Hello from the Recipe page</h4>;
+  }
+}
